2c2bda1e2382e8697e8442f80c635c54ffe53649
unknown <dungnguyen150699@gmail.com>
2023-03-31 16:55:28 +0700

    add elasticsearch

---
+++ elasticsearch/src/main/java/com/example/elasticsearch/pojo/Product.java
@@ -0,0 +1,31 @@
+package com.example.elasticsearch.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Document(indexName = "products")
+public class Product {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text, name = "name")
+    private String name;
+
+    @Field(type = FieldType.Text, name = "description")
+    private String description;
+
+    @Field(type = FieldType.Double, name = "price")
+    private double price;
+
+    
+   // Getter and Setter
+}
