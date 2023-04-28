d4319e1b00d4c036c08f383fb4ff822680f98048
unknown <dungnguyen150699@gmail.com>
2023-04-28 18:07:08 +0700

    as... d

---
+++ elasticsearch/src/test/java/com/example/elasticsearch/testApi/MockItoTest.java
@@ -0,0 +1,52 @@
+package com.example.elasticsearch.testApi;
+
+import com.example.elasticsearch.repository.ArticleRepository;
+import com.example.elasticsearch.repository.ElasticSearchHighClientQuery;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.Test;
+
+import java.io.IOException;
+
+
+// @RunWith attaches a runner with the test class to initialize the mock data
+@RunWith(MockitoJUnitRunner.class)
+public class MockItoTest {
+    // Create a mock object
+//    @Mock
+//    private UserDao userDao;
+
+//    @Mock
+//    private
+    @Mock
+    private ElasticSearchHighClientQuery elasticSearchHighClientQuery;
+    @Mock
+    private ArticleRepository articleRepository;
+
+    @Before
+    public void setUp() {
+//        userService = new UserServiceImpl(userDao);
+    }
+
+    @Test
+    public void createUser_WhenEmailExisted_ReturnFailed() throws IOException {
+        // Define return value for method createUser()
+        Mockito.lenient().when(elasticSearchHighClientQuery.searchBucketThenSourceMapping()).thenThrow(new IOException())   ;
+        Mockito.lenient().when(elasticSearchHighClientQuery.searchBucketThenSourceMapping()).thenThrow(new IOException())
+        // Use mock in test
+//        Assert.assertEquals("FAILED", userService.createUser("existed@gpcoder.com"));
+    }
+
+//    @Test
+//    public void createUser_WhenEmailNotExisted_ReturnSuccess() {
+//        // Define return value for method createUser()
+//        Mockito.when(articleRepository
+//
+//        // Use mock in test
+//        Assert.assertEquals("SUCCESS", userService.createUser("not_existed@gpcoder.com"));
+//    }
+}
